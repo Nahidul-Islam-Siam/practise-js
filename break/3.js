1de182f31bd03cceb6379b4d8ca79722253777ac
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-01-30 21:50:02 +0600

    added for

--- break/3.js
+++ break/3.js
@@ -1,3 +1,11 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+let squre;
+for (let i = 1; i <= 100;i++){
+squre = i*i
+    console.log(squre)
+    if(squre >= 100){
+        break
+    }
+}
