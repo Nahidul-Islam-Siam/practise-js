c6cc462cfd292010de37a97887ed70443b2ab1e4
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-02 23:04:26 +0600

    dey

---
+++ for/6.js
@@ -0,0 +1,10 @@
+// Reverse a string
+let sentence = "I Love You"
+let reverse = " "
+for(let i = sentence.length -1; i >= 0; i--){
+    // console.log(sentence[i]);
+    reverse += sentence[i]
+    
+    
+}console.log(reverse);
+// / console.log বাইরে দিলে দিলে ভাল হয়
--- js-string-tasks-main/5.js
+++ js-string-tasks-main/5.js
@@ -1,2 +1,5 @@
 // ### Task-5:
 // Capitalize Every first Letter of each word in a String
+let sentence = "Capitalize Every first Letter of each word in a String"
+let rape = sentence.split("  ")
+console.log(rape);
