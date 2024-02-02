0b49e3036c5010f5bdd9a28c08d3158770a34abc
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-02 20:02:21 +0600

    sss

--- Array/5.js
+++ Array/5.js
@@ -5,3 +5,7 @@
 // 1. Create two arrays of your choice.
 // 2. Use the concat method to combine the two arrays into a new array.
 // 3. Print both the original arrays and the combined array using console.log().
+const ami = ["I","Love","u"]
+const tumi = ["ami","tomake","valobashi"]
+const amra = ami.concat(tumi)
+console.log(amra);
