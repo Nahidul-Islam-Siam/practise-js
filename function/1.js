942da1a45e3a6c4d0d9efcebd20ed363100e0771
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-03 13:29:46 +0600

    ddd

--- function/1.js
+++ function/1.js
@@ -3,3 +3,10 @@
 // Take four parameters. Multiply the four numbers and then return the result 
 
 // ---
+
+function myNum(one, two, three, four){
+let number = one * two * three * four
+return number
+}
+myNum(2, 3, 2, 4)
+console.log(number);
