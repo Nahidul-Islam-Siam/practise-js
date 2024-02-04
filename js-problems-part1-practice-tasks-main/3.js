b33010d5a1f1cad070811a78c8b38a2e48a4dc0a
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-04 22:46:21 +0600

    added js task array loop

--- js-problems-part1-practice-tasks-main/3.js
+++ js-problems-part1-practice-tasks-main/3.js
@@ -3,3 +3,14 @@
 // Write a function to count the number of vowels in a string.
 
 // ---
+// function isVowel(str){
+//     let vowel = ['a','b','c','d','e']
+//     let count = 0
+//     let vowelCheck = string.match(\\gi)
+//     for(let i= 0; i<str.length;i++){
+//         if ( )
+//     }
+
+// }
+let string = "One's Upon a time there lived a beautiful woman named Cindrela"
+let str = isVowel(string)
