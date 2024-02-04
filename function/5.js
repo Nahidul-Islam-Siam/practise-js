b3fd8d4837489d8fd328b8b7c6e1b022a937a2dc
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-04 12:52:45 +0600

    function task complete

--- function/5.js
+++ function/5.js
@@ -1,16 +1,11 @@
 // ### Task-5 
 // Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return `Even`. If odd return `Odd`
-function odd_even(number) {
-    // Check if the number is even
-    if (number % 2 === 0) {
-        return "Even";
-    } else {
-        // Number is odd
-        return "Odd";
-    }
+function odd_even(integer) {
+    if(num % 2 === 1){
+        return "Odd"
+    }else
+    return "Even"
 }
-
-// Example usage:
-const testNumber = 7;
-const result = odd_even(testNumber);
-console.log(result);  // Output: Odd
+let num = 11
+let result = odd_even(num)
+console.log(result);
