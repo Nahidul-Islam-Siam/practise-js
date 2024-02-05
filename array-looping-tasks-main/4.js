4c16ae668ba862254dbe90f0fbec8bb940c685bf
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-05 17:07:45 +0600

    added part 2 task

--- array-looping-tasks-main/4.js
+++ array-looping-tasks-main/4.js
@@ -9,5 +9,37 @@
 // **Output:**
 
 // `'person working hard a am I'`
-
+// person working hard a am I
 // ---
+// ******************reverse a string 1 *****************************
+// const statement = 'I am a hard working person'
+// let number = ''
+// for(let i = statement.length - 1; i >= 0;i--){
+//      number += statement[i]
+    
+   
+// }
+// console.log(number);
+// **************************************************
+
+
+
+// ******************reverse a string 2 *****************************
+// Reverse a String With Built-In Functions
+// For this solution, we will use three methods: the String.prototype.split() method, the Array.prototype.reverse() method and the Array.prototype.join() method.
+
+
+// The split() method splits a String object into an array of string by separating the string into sub strings.
+// The reverse() method reverses an array in place. The first array element becomes the last and the last becomes the first.
+// The join() method joins all elements of an array into a string.
+
+const statement = 'I am a hard working person'
+var splitStr = statement.split(' ') 
+// [ 'I', 'am', 'a', 'hard', 'working', 'person' ]
+var reverseStr = splitStr.reverse()
+// [ 'person', 'working', 'hard', 'a', 'am', 'I' ]
+var joinStr = reverseStr.join(' ')
+// person working hard a am I
+console.log(joinStr);
+// console.log(splitStr);
+// console.log(reverseStr);
