db3bd9a28db6e52ca96e2257bb2b682231dfa56a
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-06 17:17:01 +0600

    task 3 done

--- js-problems-part2-practice-tasks-main/3.js
+++ js-problems-part2-practice-tasks-main/3.js
@@ -6,5 +6,16 @@
 //         mobile = 20000 tk
 
 // Write a JavaScript function named `calculateElectronicsBudget` that takes in the number of `laptop, tablets, and mobile` and returns the `total money required`.
-
-// ---
+ 
+function calculateElectronicsBudget(num1, num2, num3) {
+    const laptop = 35000 
+const tablet = 15000 
+const mobile = 20000 
+const laptopPrize = laptop * num1
+const tabletPrize = tablet * num2
+const mobilePrize = mobile * num3
+let totalPrize = laptopPrize + tabletPrize + mobilePrize
+return totalPrize 
+}
+let total = calculateElectronicsBudget(0,2,1)
+console.log("total money required" ,total);
