f3e972376fdfb139cb10489c3ed87e9171fd06f8
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-06 16:38:40 +0600

    task 1 & 2 done

--- js-problems-part2-practice-tasks-main/1.js
+++ js-problems-part2-practice-tasks-main/1.js
@@ -1,6 +1,29 @@
-// ### Task -1: 
+// ### Task -1:
 // Find the lowest number in the array below.
 // <br>
 // `const heights2 = [167, 190, 120, 165, 137];`
 
 // ---
+// ************************ solve 1 ******************************
+// const heights2 = [167, 190, 120, 165, 137];
+// let lowestNum = Math.min(...heights2)
+// console.log("The lowest num is " ,lowestNum);
+
+// **************************************************************
+
+// ************************ solve 2 ******************************
+
+const heights2 = [167, 190, 120, 165, 137];
+
+function lowestNum(number) {
+  let min = number[0];
+  for (let num of number) {
+    if (num < min) {
+      min = num;
+    }
+  }
+  return min
+}
+
+let low = lowestNum(heights2);
+console.log(low);
--- js-problems-part2-practice-tasks-main/2.js
+++ js-problems-part2-practice-tasks-main/2.js
@@ -1,6 +1,23 @@
-// ### Task -2: 
+// ### Task -2:
 // Find the friend with the smallest name.
 // <br>
 // `const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];`
 
 // ---
+
+let heights2 = ["rahim", "robin", "rafi", "ron", "rashed"];
+
+function smallName(friends) {
+  let found = friends[0];
+  for (let friend of friends) {
+    // console.log(friend);
+    // console.log(friend.length);
+
+    if (found.length > friend.length) {
+      found = friend;
+    }
+  }
+  return found;
+}
+let small = smallName(heights2);
+console.log(small);
