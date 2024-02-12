1cdfcfcc2ccb7d3c7c24daf6535d45c4a1c7f2ea
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-12 08:25:59 +0600

    addded dom baba

--- js-loop-main/break.js
+++ js-loop-main/break.js
@@ -1,18 +1,48 @@
-for (let i = 0; i < 150; i++) {
+// for (let i = 0; i < 150; i++) {
+//     console.log(i);
+//     if (i >= 5) {
+//         break;
+//     }
+// }
+
+// console.log('life after break up');
+
+// let n = 54;
+// while (n > 25) {
+
+//     if (n < 50) {
+//         break;
+//     }
+//     console.log(n);
+//     n--;
+// }
+// let even = []
+// let B = [2,3,4,5,6]
+// for (const Bi of B) {
+   
+//     if(Bi % 2 == 0){
+//         even.push(Bi)
+       
+//     }
+  
+//     // console.log(Bi);
+// }
+// console.log(even);
+
+// let i = 1
+// while (i <= 100) {
+   
+//     if ( i > 50) {
+//         break
+//     }
+//     console.log(i);
+//     i++
+// }
+
+for (let i = 0; i <= 100; i++) {
+    if (i > 50) {
+        break
+    }
     console.log(i);
-    if (i >= 5) {
-        break;
-    }
-}
-
-console.log('life after break up');
-
-let n = 54;
-while (n > 25) {
-
-    if (n < 50) {
-        break;
-    }
-    console.log(n);
-    n--;
+    
 }
