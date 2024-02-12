6d4a0dd7f78253a43722da969b59d8df76bbef64
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-02-12 13:05:29 +0600

    adde tasks

--- tour-of-dom-main/js/styleInJs.js
+++ tour-of-dom-main/js/styleInJs.js
@@ -1,4 +1,5 @@
 const sections = document.querySelectorAll('section');
+// console.log(sections);
 for(const section of sections){
     section.style.border = '2px solid steelblue';
     section.style.marginBottom = '5px';
@@ -6,10 +7,17 @@ for(const section of sections){
     section.style.paddingLeft = '7px';
     section.style.backgroundColor = 'lightgray';
 }
+// for(const section of sections){
+//     section.style.border = '2px solid steelblue';
+//     section.style.marginBottom = '5px';
+//     section.style.borderRadius = '15px';
+//     section.style.paddingLeft = '7px';
+//     section.style.backgroundColor = 'lightgray';
+// }
+
+// // const placesContainer = document.getElementById('places-container');
+// // placesContainer.style.backgroundColor = 'yellow';
 
 // const placesContainer = document.getElementById('places-container');
-// placesContainer.style.backgroundColor = 'yellow';
-
-const placesContainer = document.getElementById('places-container');
-placesContainer.classList.add('text-center');
-placesContainer.classList.remove('large-text')
+// placesContainer.classList.add('text-center');
+// placesContainer.classList.remove('large-text')
